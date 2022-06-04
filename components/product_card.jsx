41cc35b551486b17dc3b3bc96ec68d5de2af3fc3
ThomasCanCode <thomas@viral-dev.com>
2022-06-05 01:35:20 +0300

    pre persistant

--- components/product_card.jsx
+++ components/product_card.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { addToCart } from '../redux/cart.slice';
 import parse from 'html-react-parser';
 
-
+z
 
 function changeActive(e){ 
     const elems = document.querySelectorAll(".product_images")
@@ -15,15 +15,23 @@ function changeActive(e){
     images[e].classList.add("active_image")
 }
 
-function formSubmit(event){
-
-    event.preventDefault();
-}
 
 export default function Product_page({ product }) {
     const dispatch = useDispatch();
 
     
+function formSubmit(event){
+    event.preventDefault();
+
+    let first = event.target[0]
+    let second = event.target[1]
+    let third = event.target[2]
+    let fourth = event.target[3]
+
+    let formData = [first.value,second.value,third.value,fourth.value]
+    dispatch(addToCart({product,options:formData}))
+
+}
     
     return (
         
@@ -85,40 +93,8 @@ export default function Product_page({ product }) {
                                 )
                             }
                         })}
-                        {/* <div className={styles.individual_option}>
-                            <label htmlFor="gender">GENDER:</label>
-                            <select defaultValue={'DEFAULT'} id="gender" name="gender">
-                                <option value="DEFAULT" disabled></option>
-                                <option>option 1</option>
-                                <option>option 2</option>
-                            </select>
-                        </div>
-                        <div className={styles.individual_option}>
-                            <label htmlFor="size">SHOE SIZE:</label>
-                            <select defaultValue={'DEFAULT'} id="size" name="size">
-                                <option value="DEFAULT" disabled></option>
-                                <option>option 1</option>
-                                <option>option 2</option>
-                            </select>
-                        </div> */}
-                        {/* <div className={styles.individual_option}>
-                            <label htmlFor="receipt">RECEIPT NUMBER:</label>
-                            <select defaultValue={'DEFAULT'} id="receipt" name="receipt">
-                                <option value="DEFAULT" disabled></option>
-                                <option>option 1</option>
-                                <option>option 2</option>
-                            </select>
-                        </div>
-                        <div className={styles.individual_option}>
-                            <label htmlFor="payment">PAYMENT MADE TO:</label>
-                            <select defaultValue={'DEFAULT'} id="payment" name="payment">
-                                <option value="DEFAULT" disabled></option>
-                                <option>option 1</option>
-                                <option>option 2</option>
-                            </select>
-                        </div> */}
                         <button
-                            onClick={() => dispatch(addToCart(product))}
+                            type="submit"
                             className={styles.white_btn}
                         >
                             ADD TO MY BAG
@@ -131,16 +107,7 @@ export default function Product_page({ product }) {
 
         <div className={styles.product_page_text_container}>
             <p>{parse(product.description)}</p>
-            <h4>HOW TO ORDER</h4>
-            <p>Fill out the Drop down menu<br/>
-            Select the NFT you want on the item <br/>
-            Make payment to the Crypto Girl Wallet: 0x....3r5fwsdf <br/>
-            Attach your TXN number in this form <br/> 
-            A member from Crypto GIrl will reach out with a draft <br/> 
-            Once approved, work begins on your item <br/>
-            Please allow 4-6 weeks for all custom merchandise to be completed and shipped out from the time of the final confirmation of the design.
-            <br/> <br/> 
-            Please note, as these are HAND PAINTED pieces, no item is identical and there may be slight differences from the draft sketch and the final prodcuts. All sales are final with no refunds for this specific service.</p>
+            <p>Please note, as these are HAND PAINTED pieces, no item is identical and there may be slight differences from the draft sketch and the final prodcuts. All sales are final with no refunds for this specific service.</p>
         </div>
            
         </div>
